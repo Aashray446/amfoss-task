6fa84b698450d38433a8899608be0694a8a4e613
Aashraya Katiyar <aashraykatiyar.geek@gmail.com>
2021-10-25 22:32:13 +0530

    Task-8 All Completed

--- task-8/page-1/js/validation.js
+++ task-8/page-1/js/validation.js
@@ -1,3 +1,22 @@
-validate_email = function() {
-    console.log("successfull");
+var message = ""
+
+check_validity = function() {
+    if(!(document.getElementById("email").validity.valid)) {
+        message = "Please Enter Proper Email"
+        return false
+    }
+    if(!(document.getElementById("phone-number").validity.valid)) {
+        message =  "Please Enter Proper Number"
+        console.log(document.getElementById("phone-number").validity.valid)
+        return false
+    }
+    if((document.getElementById("name").value == "")) {
+        message = "Please  Enter The Name"
+        return false
+    }
+    if((document.getElementById("home-address").value == "")) {
+        message = "Please Enter Address"
+        return false
+    }
+    return true
 }
