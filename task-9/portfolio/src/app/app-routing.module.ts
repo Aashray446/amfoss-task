49f590c9624b7a4729c6fd269c302f9f863acde0
Aashraya Katiyar <aashraykatiyar.geek@gmail.com>
2021-11-13 18:39:35 +0530

    Education Structure Completed

--- task-9/portfolio/src/app/app-routing.module.ts
+++ task-9/portfolio/src/app/app-routing.module.ts
@@ -1,17 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EducationComponent } from './education/education.component';
-import { ExpComponent } from './exp/exp.component';
 import { HomeComponent } from './home/home.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { TestimonialsComponent } from './testimonials/testimonials.component';
 
 const routes: Routes = [
   { component: HomeComponent, path: ''},
-  {component: EducationComponent, path: 'education'},
-  {component: ExpComponent, path: 'experience'},
-  {component: ProjectsComponent, path: 'projects'},
-  {component: TestimonialsComponent, path: 'clients'}
+  {component: EducationComponent, path: 'About'},
+  {component: ProjectsComponent, path: 'Projects'},
+  {component: TestimonialsComponent, path: 'Clients'}
 ];
 
 @NgModule({
--- task-9/portfolio/src/app/app.module.ts
+++ task-9/portfolio/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { ExpComponent } from './exp/exp.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { EducationComponent } from './education/education.component';
 import { TestimonialsComponent } from './testimonials/testimonials.component';
@@ -14,7 +13,6 @@ import { ContactsComponent } from './contacts/contacts.component';
   declarations: [
     AppComponent,
     HomeComponent,
-    ExpComponent,
     ProjectsComponent,
     EducationComponent,
     TestimonialsComponent,
